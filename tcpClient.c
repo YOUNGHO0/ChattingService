9de81f5df966f0ca6e3c3f9f4ba0108a9ef8929c
YOUNGHO0 <31pqopq13@gmail.com>
2024-09-11 16:52:38 +0900

    Fix: fork 베이스로 변경

--- tcpClient.c
+++ tcpClient.c
@@ -1,90 +1,95 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-#include <fcntl.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <stdlib.h>
+#include <fcntl.h>
 #include <errno.h>
 
 #define TCP_PORT 5100
-#define PIPE_READ 0
-#define PIPE_WRITE 1
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
+    printf("This is a fork-based client\n");
     int ssock;
-
     struct sockaddr_in servaddr;
-    int pipefd[2];  // 파이프 파일 디스크립터
     char mesg[BUFSIZ];
-    ssize_t n;
 
     if (argc < 2) {
-        printf("Usage : %s IP_ADDRESS\n", argv[0]);
+        printf("Usage: %s IP_ADDRESS\n", argv[0]);
         return -1;
     }
 
-    // 파이프 생성
-    if (pipe(pipefd) < 0) {
-        perror("pipe()");
-        return -1;
-    }
-
+    /* 소켓을 생성 */
     if ((ssock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("socket()");
         return -1;
     }
 
+    /* 소켓이 접속할 주소 지정 */
     memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
+
+    /* 문자열을 네트워크 주소로 변경 */
     inet_pton(AF_INET, argv[1], &(servaddr.sin_addr.s_addr));
     servaddr.sin_port = htons(TCP_PORT);
 
+    /* 지정한 주소로 접속 */
     if (connect(ssock, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
         perror("connect()");
-        close(ssock);
         return -1;
     }
 
-    // 입력 스트림을 파이프의 쓰기 끝으로 리디렉션
-    dup2(pipefd[PIPE_WRITE], STDOUT_FILENO);
-    close(pipefd[PIPE_WRITE]);
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror("fork()");
+        return -1;
+    }
 
-    // 데이터 수신을 처리하는 자식 프로세스 생성
-    if (fork() == 0) {
-        close(pipefd[PIPE_READ]); // 자식 프로세스는 읽기 끝을 사용하지 않음
+    if (pid == 0) {
+        // 자식 프로세스: 서버로부터 데이터 받기
         while (1) {
-            memset(mesg, 0, BUFSIZ);
-            n = recv(ssock, mesg, BUFSIZ, 0);
-            if (n > 0) {
-                printf("Received data: %s", mesg);
-            } else if (n == 0) {
-                break;
-            } else if (errno != EAGAIN && errno != EWOULDBLOCK) {
-                perror("recv()");
-                break;
+            int n = recv(ssock, mesg, sizeof(mesg) - 1, 0);
+            if (n <= 0) {
+                if (n == 0) {
+                    printf("서버가 연결을 종료했습니다.\n");
+                } else {
+                    perror("recv()");
+                }
+                close(ssock);
+                exit(0);
             }
-            usleep(100000);  // Sleep for 100 milliseconds
+            mesg[n] = '\0'; // 받은 메시지 null 종료
+            printf("%s", mesg); // 서버에서 받은 메시지 출력
+            fflush(stdout); // 출력 버퍼를 즉시 비우기
         }
-        close(ssock);
-        _exit(0);
-    }
+    } else {
+        // 부모 프로세스: 키보드 입력 받기
+        // 표준 입력을 논블로킹 모드로 설정
+        int flags = fcntl(STDIN_FILENO, F_GETFL, 0);
+        if (flags < 0) {
+            perror("fcntl(F_GETFL)");
+            return -1;
+        }
+        if (fcntl(STDIN_FILENO, F_SETFL, flags | O_NONBLOCK) < 0) {
+            perror("fcntl(F_SETFL)");
+            return -1;
+        }
 
-    // 부모 프로세스는 파이프의 읽기 끝을 사용하여 송신을 처리
-    close(pipefd[PIPE_WRITE]); // 부모 프로세스는 쓰기 끝을 사용하지 않음
-    while (1) {
-        if (fgets(mesg, BUFSIZ, stdin) != NULL) {
-            if (write(pipefd[PIPE_READ], mesg, strlen(mesg)) < 0) {
-                perror("write()");
-                break;
+        while (1) {
+            ssize_t len = fgets(mesg, sizeof(mesg), stdin) != NULL ? strlen(mesg) : 0;
+            if (len > 0) {
+                // 소켓으로 데이터를 비동기로 전송
+                if (send(ssock, mesg, len, MSG_DONTWAIT) <= 0) {
+                    perror("send()");
+                    close(ssock);
+                    exit(0);
+                }
             }
-            if (strncmp(mesg, "...", 3) == 0) {
-                break;
-            }
+            usleep(100000); // 0.1초 지연
         }
     }
-
-    close(pipefd[PIPE_READ]);
-    close(ssock);
 
     return 0;
 }
