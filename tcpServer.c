cf02390323d98b3a5a168ed7f016874d5dd9143f
YOUNGHO0 <31pqopq13@gmail.com>
2024-09-11 14:43:45 +0900

    Fix : read시 자식프로세스 FD논블록 처리

--- tcpServer.c
+++ tcpServer.c
@@ -6,7 +6,8 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdlib.h>
-
+#include <fcntl.h>
+#include <errno.h>
 #define TCP_PORT 5100 /* 서버의 포트 번호 */
 #define MAX_CLIENTS 100 /* 최대 클라이언트 수 */
 
@@ -35,6 +36,17 @@ int main(int argc, char **argv) {
         return -1;
     }
 
+    /* 소켓을 non-blocking 모드로 설정 */
+    int flags = fcntl(ssock, F_GETFL, 0);
+    if (flags < 0) {
+        perror("fcntl(F_GETFL)");
+        return -1;
+    }
+    if (fcntl(ssock, F_SETFL, flags | O_NONBLOCK) < 0) {
+        perror("fcntl(F_SETFL)");
+        return -1;
+    }
+
     /* 동시에 접속하는 클라이언트의 처리를 위한 대기 큐를 설정 */
     if (listen(ssock, 8) < 0) {
         perror("listen()");
@@ -47,27 +59,66 @@ int main(int argc, char **argv) {
         /* 클라이언트가 접속하면 접속을 허용하고 클라이언트 소켓 생성 */
         int csock = accept(ssock, (struct sockaddr *)&cliaddr, &clen);
 
+        printf("접속됨:csock : %d : sscok : %d\n", csock,ssock);
         if (csock < 0) {
-            perror("accept()");
-            continue;
+            if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                printf("읽기 수행중 \n");
+                sleep(3);
+                // 부모 프로세스는 읽기 디스크립터를 사용하여 자식의 데이터를 읽음
+                for(int i =0; i<=client_count; i++){
+
+                    int n = read(pipe_fd[i][0], mesg, BUFSIZ);
+                    if (n > 0) {
+                        mesg[n] = '\0';
+                        printf("Parent received from child: %s\n", mesg); // 자식 프로세스의 메시지를 출력
+                    }
+
+//                    else if (n == 0) {
+//                        // 파이프가 닫혔다는 것은 자식 프로세스가 종료되었음을 의미
+//                        break;
+//                    } else {
+//                        perror("readddsss()");
+//                        break;
+//                    }
+
+                }
+                continue;
+                // 클라이언트 연결이 없을 경우, 다른 작업 수행
+            } else {
+                perror("accept()");
+                continue;
+            }
         }
+
+
 
         if (client_count >= MAX_CLIENTS) {
             printf("Max clients reached. Connection rejected.\n");
             close(csock);
             continue;
         }
+        printf("client Count : %d\n", client_count);
 
         /* 새로운 파이프 생성 */
         if (pipe(pipe_fd[client_count]) == -1) {
             perror("pipe");
             return -1;
         }
+        // pipe_fd[i][0]을 비동기 모드로 설정
+        int flags = fcntl(pipe_fd[client_count][0], F_GETFL, 0);
+        if (flags == -1) {
+            perror("fcntl F_GETFL");
+            exit(EXIT_FAILURE);
+        }
+        if (fcntl(pipe_fd[client_count][0], F_SETFL, flags | O_NONBLOCK) == -1) {
+            perror("fcntl F_SETFL");
+            exit(EXIT_FAILURE);
+        }
 
         if (fork() == 0) { // 자식 프로세스
             close(pipe_fd[client_count][0]); // 읽기 디스크립터 닫기 (자식은 쓰기 전용)
 
-            close(ssock); // 자식 프로세스는 서버 소켓을 사용하지 않음
+//            close(ssock); // 자식 프로세스는 서버 소켓을 사용하지 않음
 
             /* 네트워크 주소를 문자열로 변경 */
             inet_ntop(AF_INET, &cliaddr.sin_addr, mesg, BUFSIZ);
@@ -75,10 +126,12 @@ int main(int argc, char **argv) {
             fflush(stdout); // 버퍼에 있는 데이터를 강제로 출력
 
             while (1) {
-                printf("child fd = %d\n", client_count);
+//                printf("child fd = %d\n", client_count);
+//                printf("read 수행 전\n");
                 int n = read(csock, mesg, BUFSIZ); // 클라이언트로부터 데이터 읽기
                 if (n <= 0) {
-                    perror("read()");
+                    continue;
+                    perror("read issue()");
                     break;
                 }
 
@@ -107,26 +160,11 @@ int main(int argc, char **argv) {
         close(pipe_fd[client_count][1]); // 부모는 쓰기 디스크립터 닫기
         printf("parent total: %d\n", client_count);
         fflush(stdout);
-        // 부모 프로세스는 읽기 디스크립터를 사용하여 자식의 데이터를 읽음
-            for(int i =0; i<=client_count; i++){
 
-                int n = read(pipe_fd[i][0], mesg, BUFSIZ);
-                if (n > 0) {
-                    mesg[n] = '\0';
-                    printf("Parent received from child: %s\n", mesg); // 자식 프로세스의 메시지를 출력
-                } else if (n == 0) {
-                    // 파이프가 닫혔다는 것은 자식 프로세스가 종료되었음을 의미
-                    break;
-                } else {
-                    perror("read()");
-                    break;
-                }
-
-            }
+        client_count++;
 
         // 좀비 프로세스 제거
-        while (waitpid(-1, NULL, WNOHANG) > 0);
-        client_count++;
+//        while (waitpid(-1, NULL, WNOHANG) > 0);
     }
 
     for (int i = 0; i < client_count; i++) {
