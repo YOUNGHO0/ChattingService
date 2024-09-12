ceee505bbdcd8d00301638d6cebf031f12db6315
YOUNGHO0 <31pqopq13@gmail.com>
2024-09-12 19:46:25 +0900

    Feat: 클라이언트 개선

--- tcpClient.c
+++ tcpClient.c
@@ -1,24 +1,67 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdbool.h>
 
+#define BUFFER_SIZE 1024
+#define MAX_MESSAGES 1000
 #define TCP_PORT 5100
 
-int main(int argc, char **argv)
-{
+// 전역 변수: 부모와 자식 프로세스의 메시지를 저장
+char parent_messages[MAX_MESSAGES][BUFFER_SIZE];
+char child_messages[MAX_MESSAGES][BUFFER_SIZE];
+int parent_msg_count = 0;
+int child_msg_count = 0;
+
+struct sockaddr_in setNetwork(char *const *argv, struct sockaddr_in *servaddr);
+
+void print_error(const char *msg);
+
+struct sockaddr_in setNetwork(char *const *argv, struct sockaddr_in *servaddr);
+
+void print_matching_messages(const char *keyword, int is_parent);
+
+int handleArgs(char *const *argv);
+
+void makeChildPipe(int *to_child);
+
+void makeParentPipe(int *to_parent);
+
+int makeServerSocketPipe(int ssock);
+
+void setNoneBlockSeverPipe(int ssock, int flags);
+
+void setChildPipeToNoneblock(const int *to_child, int flags);
+
+int setSocketPipeToNoneblock(int ssock);
+
+void receveMessageFromServer(int ssock, char *mesg);
+
+void handleSearchCommand(const int *to_child, ssize_t bytes_read);
+
+bool isSearchCommand(const char *mesg);
+
+void sendMessageToChild(const char *mesg, const int *to_child, ssize_t len);
+
+void findKeyWordInData(const char *mesg);
+
+void sendMessageToServer(int ssock, const char *mesg, ssize_t len);
+
+bool getLoggable();
+
+int main(int argc, char **argv) {
     printf("This is a fork-based client\n");
     int ssock;
     struct sockaddr_in servaddr;
     char mesg[BUFSIZ];
 
     if (argc < 2) {
-        printf("Usage: %s IP_ADDRESS\n", argv[0]);
-        return -1;
+        return handleArgs(argv);
     }
 
     /* 소켓을 생성 */
@@ -26,64 +69,71 @@ int main(int argc, char **argv)
         perror("socket()");
         return -1;
     }
-
     /* 소켓이 접속할 주소 지정 */
-    memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-
-    /* 문자열을 네트워크 주소로 변경 */
-    inet_pton(AF_INET, argv[1], &(servaddr.sin_addr.s_addr));
-    servaddr.sin_port = htons(TCP_PORT);
-
+    servaddr = setNetwork(argv, &servaddr);
     /* 지정한 주소로 접속 */
     if (connect(ssock, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
         perror("connect()");
         return -1;
     }
 
+    int to_child[2];
+    int to_parent[2];
+
+    makeChildPipe(to_child);
+    makeParentPipe(to_parent);
+    int flags = setSocketPipeToNoneblock(ssock);
+    setChildPipeToNoneblock(to_child, flags);
+
     pid_t pid = fork();
     if (pid < 0) {
         perror("fork()");
-        return -1;
+        exit(-1);
     }
 
     if (pid == 0) {
-        // 자식 프로세스: 서버로부터 데이터 받기
-        while (1) {
-            int n = recv(ssock, mesg, sizeof(mesg) - 1, 0);
-            if (n <= 0) {
-                if (n == 0) {
-                    printf("서버가 연결을 종료했습니다.\n");
-                } else {
-                    perror("recv()");
-                }
-                close(ssock);
-                exit(0);
-            }
-            mesg[n] = '\0'; // 받은 메시지 null 종료
-            printf("%s", mesg); // 서버에서 받은 메시지 출력
-            fflush(stdout); // 출력 버퍼를 즉시 비우기
-        }
-    } else {
-        // 부모 프로세스: 키보드 입력 받기
-        // 표준 입력을 논블로킹 모드로 설정
-        int flags = fcntl(STDIN_FILENO, F_GETFL, 0);
-        if (flags < 0) {
-            perror("fcntl(F_GETFL)");
-            return -1;
-        }
-        if (fcntl(STDIN_FILENO, F_SETFL, flags | O_NONBLOCK) < 0) {
-            perror("fcntl(F_SETFL)");
-            return -1;
-        }
+        close(to_child[1]);
+        close(to_parent[0]);
 
         while (1) {
+            // 자식 프로세스: 서버에서 메시지 수신 및 검색 요청 처리
+            ssize_t bytes_read;
+            receveMessageFromServer(ssock, mesg);
+            // 검색 요청 읽기
+            handleSearchCommand(to_child, bytes_read);
+            usleep(100000); // 0.1초 지연
+        }
+    } else {
+        // 부모 프로세스: 키보드 입력 받기 및 메시지 전송
+        close(to_child[0]);
+        close(to_parent[1]);
+        while (1) {
+
             ssize_t len = fgets(mesg, sizeof(mesg), stdin) != NULL ? strlen(mesg) : 0;
+
             if (len > 0) {
-                // 소켓으로 데이터를 비동기로 전송
-                if (send(ssock, mesg, len, MSG_DONTWAIT) <= 0) {
-                    perror("send()");
-                    close(ssock);
+                if (isSearchCommand(mesg)) {
+                    // 검색 요청을 자식 프로세스에 보내기
+                    sendMessageToChild(mesg, to_child, len);
+                    printf("Chat Log\n");
+                    findKeyWordInData(mesg);
+                } else {
+                    // 소켓으로 데이터를 비동기로 전송
+                    sendMessageToServer(ssock, mesg, len);
+
+                    // 부모 프로세스에서 수신된 메시지 저장
+                    if (getLoggable()) {
+                        strncpy(parent_messages[parent_msg_count++], mesg, BUFFER_SIZE);
+                    }
+                }
+
+                // 종료 조건 확인
+                if (strncmp(mesg, "...", 3) == 0) {
+                    close(to_parent[1]);
+                    close(to_parent[0]);
+                    close(to_child[1]);
+                    close(to_child[0]);
+                    printf("종료합니다.\n");
                     exit(0);
                 }
             }
@@ -92,4 +142,132 @@ int main(int argc, char **argv)
     }
 
     return 0;
+}
+
+bool getLoggable() { return parent_msg_count < MAX_MESSAGES; }
+
+void sendMessageToServer(int ssock, const char *mesg, ssize_t len) {
+    if (send(ssock, mesg, len, MSG_DONTWAIT) <= 0) {
+        if (errno != EAGAIN && errno != EWOULDBLOCK) {
+            perror("send()");
+            close(ssock);
+            exit(0);
+        }
+    }
+}
+
+void findKeyWordInData(const char *mesg) {
+    char *keyword = mesg + 7;
+    keyword[strcspn(keyword, "\n")] = '\0'; // 개행 문자 제거
+    print_matching_messages(keyword, 1);
+}
+
+void sendMessageToChild(const char *mesg, const int *to_child, ssize_t len) {
+    if (write(to_child[1], mesg, len) != len) {
+        perror("writeerror®()");
+        close(to_child[1]);
+        exit(0);
+    }
+}
+
+bool isSearchCommand(const char *mesg) { return strncmp(mesg, "search:", 7) == 0; }
+
+void handleSearchCommand(const int *to_child, ssize_t bytes_read) {
+    char res[BUFFER_SIZE];
+    bytes_read = read(to_child[0], res, sizeof(res));
+    if (bytes_read > 0) {
+        res[bytes_read] = '\0';
+        if (strncmp(res, "search:", 7) == 0) {
+            char *keyword = res + 7;
+            keyword[strcspn(keyword, "\n")] = '\0';
+            print_matching_messages(keyword, 1);
+        }
+    } else if (bytes_read < 0) {
+        if (errno != EAGAIN && errno != EWOULDBLOCK) {
+            print_error("read() from pipefd1");
+        }
+    }
+}
+
+void receveMessageFromServer(int ssock, char *mesg) {
+    ssize_t bytes_read = read(ssock, mesg,BUFSIZ);
+    if (bytes_read > 0) {
+        mesg[bytes_read] = '\0';
+        printf("%s\n", mesg);
+    } else if (bytes_read < 0) {
+        if (errno != EAGAIN && errno != EWOULDBLOCK) {
+            print_error("read() from socket");
+        }
+    }
+}
+
+int setSocketPipeToNoneblock(int ssock) {
+    int flags = fcntl(ssock, F_GETFL, 0);
+    if (flags < 0) {
+        perror("fcntl(F_GETFL)");
+        exit(-1);
+    }
+    if (fcntl(ssock, F_SETFL, flags | O_NONBLOCK) < 0) {
+        perror("fcntl(F_SETFL)");
+        exit(-1);
+    }
+    return flags;
+}
+
+void setChildPipeToNoneblock(const int *to_child, int flags) {
+    flags = fcntl(to_child[0], F_GETFL, 0);
+    if (flags < 0) {
+        perror("fcntl(F_GETFL)");
+        exit(-1);
+    }
+    if (fcntl(to_child[0], F_SETFL, flags | O_NONBLOCK) < 0) {
+        perror("fcntl(F_SETFL)");
+        exit(-1);
+    }
+}
+
+
+
+void makeParentPipe(int *to_parent) {
+    if (pipe(to_parent) == -1) {
+        perror("pipe");
+        exit(-1);
+    }
+}
+
+void makeChildPipe(int *to_child) {
+    if (pipe(to_child) == -1) {
+        perror("pipe");
+        exit(-1);
+    }
+}
+
+int handleArgs(char *const *argv) {
+    printf("Usage: %s IP_ADDRESS\n", argv[0]);
+    return -1;
+}
+
+
+void print_error(const char *msg) {
+    fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+}
+
+void print_matching_messages(const char *keyword, int is_parent) {
+    int count = is_parent ? parent_msg_count : child_msg_count;
+    char (*messages)[BUFFER_SIZE] = is_parent ? parent_messages : child_messages;
+
+    for (int i = 0; i < count; ++i) {
+        if (strstr(messages[i], keyword)) {
+            printf("%s", messages[i]);
+        }
+    }
+}
+
+struct sockaddr_in setNetwork(char *const *argv, struct sockaddr_in *servaddr) {
+    memset(servaddr, 0, sizeof(*servaddr));
+    (*servaddr).sin_family = AF_INET;
+    /* 문자열을 네트워크 주소로 변경 */
+    inet_pton(AF_INET, argv[1], &((*servaddr).sin_addr.s_addr));
+    (*servaddr).sin_port = htons(TCP_PORT);
+    return (*servaddr);
 }
