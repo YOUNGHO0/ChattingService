8eaa29fc2036544e7b264b643d150b0612e4cba5
YOUNGHO0 <31pqopq13@gmail.com>
2024-09-13 11:41:37 +0900

    채팅방 문구 추가

--- tcpServer.c
+++ tcpServer.c
@@ -259,6 +259,20 @@ void broadcastToClients(int client_count, int client_pos, userinfo *user) {
         int number = atoi(user->message + 7);
         client_csock_info[client_pos].roomNumber = number;
         printf("select room number is %d\n",number);
+        char buffer[BUFSIZ];
+        int len;
+
+        // 포맷된 문자열을 버퍼에 저장합니다.
+        len = snprintf(buffer, sizeof(buffer), "Now you are in channel %d\n start chat", number);
+        if (len < 0) {
+            perror("snprintf");
+            return;
+        }
+
+        // 포맷된 문자열을 클라이언트 소켓에 씁니다.
+        if (write(user->csockId, buffer, len) < 0) {
+            perror("broadcast write");
+        }
         return;
     }
     broadCast(client_count, user, formatted_message);
@@ -406,7 +420,7 @@ void handleClient(int csock, int client_index, int (*pipe_fd)[2], struct sockadd
     setNetworkConnection(&cliaddr, &user);
 
     handleUserLogin(csock, &user);
-    write(csock, "Input ChatRoom Number", strlen("Input ChatRoom Number"));
+    write(csock, "Input ChatRoom Number\n if you want to create use 'create:{channelName} to create channel\n", strlen("Input ChatRoom Number\n if you want to create use 'create:{channelName} to create channel\n"));
     FILE *file = open_file_in_saved_dir("chatRoom.txt", "r");
     if (file == NULL) {
         perror("fopen");
@@ -485,7 +499,7 @@ void handleClient(int csock, int client_index, int (*pipe_fd)[2], struct sockadd
 
         user.room_number=updated_room_count-1;
         printf("start6\n");
-        write(csock, "Updated Room List\n", strlen("Updated Room List\n"));
+        write(csock, "Updated Room List\n Input channel number", strlen("Updated Room List\n Input channel number"));
 
         file = open_file_in_saved_dir("chatRoom.txt", "r");
         if (file == NULL) {
@@ -518,6 +532,7 @@ void handleClient(int csock, int client_index, int (*pipe_fd)[2], struct sockadd
         snprintf(select, sizeof(select), "select:%s", chatRoomNumber);
         strcpy(user.message,select);
         write(pipe_fd[client_index][1], &user, sizeof(user));
+        sleep(1);
     }
 
 
